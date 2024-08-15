34b1d16474bd93563cd8cdc42a0201746bb01c8f
Hash Inferni <pceasabasaba@gmail.com>
2024-08-15 23:59:36 +0300

    login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,9 @@
 export default function LoginPage() {
     return (
         <div className="flex">
-            Login
+            <h1 className="text-center text-white text-2xl">
+                Login
+            </h1>
         </div>
     )
 }
