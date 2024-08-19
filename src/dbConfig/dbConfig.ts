ea3ebe50fbad1c1c284f2b16978657c7e554fbd1
Hash Inferni <pceasabasaba@gmail.com>
2024-08-19 09:02:04 +0300

    db config file

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -2,8 +2,11 @@
 
 export async function connect(){
     try {
+        mongoose.connect(process.env.MONGO_URI!);
+        const connection = mongoose.connection;
         
     } catch (error) {
-        
+        console.log('Something went wrong!');
+        console.log(error);
     }
 }
