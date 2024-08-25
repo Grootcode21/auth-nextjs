5376dfcb85efdfc03589193db7c2ac941fc271e4
Hash Inferni <antonyndichu15@gmail.com>
2024-08-26 00:57:22 +0300

    id user profile

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,9 +1,9 @@
-export default function ProfilePage (){
+export default function UserProfile ({params}: any){
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <h1>Profile</h1>
         <hr />
-        <p>Profile page</p>
+        <p className="text-4xl">Profile page{params.id}</p>
         </div>
 
     )
