f3c7508b9bc7d033beb7fddd2760839ac702519b
Grootcode21 <antonyndichu15@gmail.com>
2024-09-07 23:37:28 -0700

    user models

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,7 +1,29 @@
 import mongoose from "mongoose"
 
 const userSchema = new mongoose.Schema({
-
+    username: {
+        type: String,
+        required: [true,"Please enter a username"],
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true,"Please enter a email"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true,"Please enter a password"],
+        minlength: [8,"Password must be at least 8 characters long"]
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    }
 
 
 })
