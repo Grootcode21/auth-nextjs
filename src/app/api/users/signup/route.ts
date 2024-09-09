dcfe1126cd51b6be2761c72817309ea698c684bb
Grootcode21 <antonyndichu15@gmail.com>
2024-09-09 00:14:41 -0700

    signup route

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -10,7 +10,17 @@ export async function POST(request: NextRequest) {
     try {
         const reqBody = await request.json();
         const {username, email, password} = reqBody;
+
+        console.log(reqBody);
+
+        //check if user already exists
+        const user = await User.findOne({email})
+
+        if (user){
+            return NextResponse.json({error: "User already exists"},{status:400})
+        }
+
     } catch (error: any){
-        return NextResponse.status(500).json({message: error.message})
+        return NextResponse.json({error: error.message},{status:500})
     }
 }
